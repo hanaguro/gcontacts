b08594f0c64c12a26deeb169e5bd79da5d564ce2
Takahiro Yoshizawa <kuro@takahiro.org>
2023-12-25 01:04:03 +0900

    ニックネームの取得方法を変更

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,6 @@ use hyper::client::Client; // HTTPクライアント操作用
 use hyper_rustls::HttpsConnector; // HTTPSサポート用
 use google_people1::{PeopleService, FieldMask}; // Google People APIを使用するため
 use std::str::FromStr; // 文字列を型に変換するため
-use std::collections::HashSet; // データに対する集合演算用
 use csv::WriterBuilder; // CSVファイルを書き込むため
 use std::io; // 入出力機能のための 'io' モジュールをインポート
 use std::path::Path; // ファイルパスを扱うための 'Path' モジュールをインポート
@@ -39,26 +38,81 @@ fn print_help(bundle: &FluentBundle<FluentResource, IntlLangMemoizer>) {
     // ここに他の詳細な説明や使用方法を記述する
 }
 
-// 与えられた名前とメールアドレスの数に基づいてニックネームを生成する関数
-fn generate_nickname(name: &str, email_count: usize, existing_nicknames: &mut HashSet<String>) -> String {
-    // 名前の最後の部分（姓と仮定）を取得し、基本的なニックネームを作成
-    let last_name_part = name.split_whitespace().last().unwrap_or("Unknown").to_string();
-    let base_nickname = last_name_part;
+/// 文字列内で最初に数字が現れる部分を見つけ、文字列部分と数値部分に分割する。
+///
+/// # 引数
+/// * `s` - 分割対象の文字列。
+///
+/// # 戻り値
+/// 文字列部分と数値部分を含むタプル。数値部分が存在しない場合は0を返す。
+fn split_string_and_number(s: &str) -> (String, u32) {
+    // 数字が始まるインデックスを保存するための変数
+    let mut num_start_index = None;
 
-    // 複数のメールアドレスがある場合、一意のニックネームを生成
+    // 文字列の各文字に対してループを行う
+    for (index, character) in s.char_indices() {
+        // 文字が数字かどうかをチェック
+        if character.is_digit(10) {
+            // 数字が見つかった場合、そのインデックスを保存しループを抜ける
+            num_start_index = Some(index);
+            break;
+        }
+    }
+
+    // 数字が見つかった場合の処理
+    match num_start_index {
+        // 数字が見つかった場合
+        Some(index) => {
+            // 文字列を数字が始まる箇所で分割する
+            let string_part = &s[..index];
+            let number_part = &s[index..];
+            // 文字列部分と数値部分をタプルとして返す
+            (string_part.to_string(), number_part.parse().unwrap_or(0))
+        },
+        // 数字が見つからなかった場合
+        None => (s.to_string(), 0),
+    }
+}
+
+/// 与えられた名前とメールアドレスの数に基づいてニックネームを生成する。
+///
+/// # 引数
+/// * `name` - 名前の文字列。
+/// * `email_count` - メールアドレスの数。
+/// * `existing_nicknames` - 既に存在するニックネームのリストへの可変参照。
+///
+/// # 戻り値
+/// 生成されたニックネームの文字列。
+fn generate_nickname(name: &str, email_count: usize, existing_nicknames: &mut Vec<String>) -> String {
+    // 名前の最後の部分を取得し、基本的なニックネームを作成
+    let last_name_part = name.split_whitespace().last().unwrap_or("Unknown").to_string();
+    let base_nickname;
+    let mut counter;
+
+    // 既に存在するニックネームがある場合は、その最初のニックネームから数値部分を抽出
+    if !existing_nicknames.is_empty(){
+        (base_nickname, counter) = split_string_and_number(existing_nicknames[0].as_str());
+    } else {
+        // 既存のニックネームがない場合は、名前の最後の部分を基本ニックネームとして使用
+        base_nickname = last_name_part;
+        counter = 0;
+    }
+
+    // メールアドレスが複数ある場合、一意のニックネームを生成
     if email_count > 1 {
-        let mut counter = 1;
-        let mut nickname = format!("{}{:02}", base_nickname, counter);
-        // 既に存在するニックネームと重複しないようにカウンターを増やしながらニックネームを生成
+        // 基本ニックネームに数値を追加してニックネームを生成
+        let mut nickname = format!("{}{:02}", base_nickname, counter + 1);
+        // 既存のニックネームと重複しないようにカウンターを増やしながらニックネームを生成
         while existing_nicknames.contains(&nickname) {
             counter += 1;
-            nickname = format!("{}{:02}", base_nickname, counter);
+            nickname = format!("{}{:02}", base_nickname, counter + 1);
         }
-        existing_nicknames.insert(nickname.clone());
+        // 生成されたニックネームをリストに追加
+        existing_nicknames.push(nickname.clone());
         nickname
     } else {
-        // メールアドレスが1つだけの場合は基本的なニックネームを使用
-        existing_nicknames.insert(base_nickname.clone());
+        // メールアドレスが1つのみの場合は基本ニックネームを使用
+        existing_nicknames.push(base_nickname.clone());
         base_nickname
     }
 }
@@ -95,7 +149,7 @@ async fn main(){
     let service = PeopleService::new(Client::builder().build(HttpsConnector::with_native_roots()), auth);
 
     // Google People APIから取得するフィールドを設定
-    let field_mask = FieldMask::from_str("names,emailAddresses").unwrap(); // 失敗したらパニック
+    let field_mask = FieldMask::from_str("nicknames,names,emailAddresses").unwrap(); // 失敗したらパニック
 
     // Google People APIを使用して連絡先情報を取得
     // resultsは(Response<Body>, ListConnectionsResponse)のタプル
@@ -108,8 +162,9 @@ async fn main(){
         });
 
 
-    // 生成されたニックネームを格納するHashSet
-    let mut existing_nicknames = HashSet::new();
+    // 生成されたニックネームを格納するVec
+    let mut existing_nicknames = Vec::new();
+
     // CSVファイルの保存場所を指定
     let home_dir = dirs::home_dir().unwrap_or_else(|| {
         eprintln!("{}",mod_fluent::get_translation(&bundle, "home-notfound"));
@@ -119,7 +174,7 @@ async fn main(){
 
     let addressbook_path = home_dir.join(".addressbook");
 
-    // ファイルが存在するかチェック
+    // ファイルが存在するかチェックし、存在する場合は上書き確認する
     if Path::new(&addressbook_path).exists() {
         println!("{}",mod_fluent::get_translation(&bundle, "overwrite-or-not"));
         let mut input = String::new();
@@ -146,12 +201,25 @@ async fn main(){
     // 取得した連絡先情報に基づいて処理
     if let Some(connections) = results.1.connections {
         for person in connections {
-            // 各人物の名前とメールアドレスを取得
+            // 各人物のニックネームと名前とメールアドレスを取得
+            let mut nicknames = person.nicknames.unwrap_or_else(Vec::new);
+            if nicknames.is_empty() {
+                nicknames = Vec::new();
+            }
             let names = person.names.unwrap_or_else(Vec::new);
             let emails = person.email_addresses.unwrap_or_else(Vec::new);
 
             // 名前が存在する場合のみ処理
             if !names.is_empty() {
+                if nicknames.len() != 0 {
+                    let nickname = nicknames[0].value.as_ref().map(|s| s.as_str()).unwrap_or("");
+                    if nickname != "" {
+                        existing_nicknames.insert(0, nickname.to_string());
+                    }
+                }else{
+                    existing_nicknames = Vec::new();
+                }
+
                 let name = names[0].display_name.as_ref().map(|s| s.as_str()).unwrap_or("default");
                 let email_count = emails.len();
 
